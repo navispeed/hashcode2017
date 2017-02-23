95759506e503a71bf77ba0658b35554d1e5d5f54
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2017-02-23 22:09:55 +0100

    basic OK

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
 var fs = require('fs');
 
-function WriteFile(inputs) {
+function WriteFile(inputs, output) {
     var data = howManyDoWeUse(inputs) + "\n";
 
     var caches = inputs.caches;
-    for (var i = 0; i < caches.length; ++i) {
-        var line = caches[i].id + " ";
-        if (caches[i].used == 0) {
+    for (var elem in caches) {
+        var line = caches[elem].id;
+        if (caches[elem].used == 0) {
             continue;
         }
-        for (var j = 0; j < caches[i].videos.length; ++j) {
-            line += " " + caches[i].videos[j].id
+        for (var j = 0; j < caches[elem].videos.length; ++j) {
+            line += " " + caches[elem].videos[j].id
         }
         line += "\n";
         data += line;
     }
+    for (var i = 0; i < caches.length; ++i) {
 
-    fs.writeFile("./res", data);
+    }
+    fs.writeFile(output.split("/")[1], data);
 }
 
 //How to include file in nodejs =>
 // eval(fs.readFileSync("src/proto.js") + '');
 eval(fs.readFileSync("src/input.js") + '');
+
+if (process.argv.length < 2) {
+    return;
+}
 var file = fs.readFileSync(process.argv[2]) + '';
 
 
 // console.log(file.split("\n"));
 
 var input = new Input(file.split("\n"));
+
+for (var i = 0; i < input.ep.length; ++i) {
+    input.ep[i].createAllRequests();
+    input.ep[i].updateAllRequests();
+    console.log("Boucle 1")
+}
+
+for (var elem in input.caches) {
+    input.caches[elem].fill(getRequestofCache(input.caches[elem]));
+    console.log("Boucle 2")
+}
+
+WriteFile(input, process.argv[2] + ".res");
--- src/input.js
+++ src/input.js
@@ -8,28 +8,33 @@ function getRequestofCache(cache) {
     var tab = [];
 
     for (var i = 0; i < cache.endPoints.length; ++i) {
-        tab.concat(cache.endPoints[i].getAllRequest());
-    }
+        var tmp = cache.endPoints[i].getAllRequest();
 
+        for (var j = 0; j < tmp.length; ++j) {
+            tab.push(tmp[j]);
+        }
+    }
     return tab;
 }
 
 function Request(requests, latency) {
     this.nbRequest = requests.nbRequest;
     this.videoId = requests.videoId;
-    this.videoSize = input.videos[requests.videoId];
-    this.weight = this.nbRequest * this.videoSize * latency;
+    this.videoSize = input.videos[requests.videoId].size;
+    this.weight = parseInt(this.nbRequest) * parseInt(this.videoSize) * parseInt(latency);
     this.latency = latency;
-    this.recalculateWeight = function (latency){
-        this.weight = this.nbRequest * this.videoSize * latency;
-    }
+    this.recalculateWeight = function (_latency){
+        this.weight = parseInt(this.nbRequest) * parseInt(this.videoSize) * parseInt(_latency);
+    };
     this.calculateLatency = function (servers, endpoint) {
         this.latency = endpoint.latency;
-        for (var server in servers)
+        for (var _server in servers)
         {
-            for (var video in server.videos)
+            var server = servers[_server];
+            for (var _video in server.videos)
             {
-                if (videoId == video.videoId)
+                var video = server.videos[_video];
+                if (this.videoId == video.videoId)
                 {
                     if (server.latency[endpoint.id] < this.latency)
                     {
@@ -39,6 +44,7 @@ function Request(requests, latency) {
                 }
             }
         }
+        this.recalculateWeight(this.latency);
     }
 }
 
@@ -64,18 +70,18 @@ function EndPoints(id, latency) {
     this.requests = [];
     this.Requests = [];
     this.createAllRequests = function() {
-        Requests = [];
+        this.Requests = [];
         for (var request in this.requests) {
-            Requests.push(new Request(requests[request], latency));
+            this.Requests.push(new Request(this.requests[request], latency));
         }
     };
     this.updateAllRequests = function () {
-        for (var request in this.requests) {
-            Requests.push(new Request(requests[request], latency));
-            requests[request].calculateLatency(cache);
+        for (var request in this.Requests) {
+            this.Requests[request].calculateLatency(this.cache, this);
         }
     };
     this.getAllRequest = function() {
+        // console.log(this.Requests);
         return this.Requests.sort(function(a, b) {
             if (a.weight < b.weight)
                 return -1;
@@ -95,13 +101,14 @@ function Cache(id, maxSize) {
     this.endPoints = [];
     this.fill = function (requests)
     {
-        for (var request in requests)
+        for (var _request in requests)
         {
-            if (this.capacity - this.used >= request.video.size)
+            var request = requests[_request];
+            if (this.capacity - this.used >= input.videos[request.videoId].size)
             {
-                this.videos.push(request.video);
-                request.video.servers.push(this);
-                this.used += request.video.size;
+                this.videos.push(input.videos[request.videoId]);
+                input.videos[request.videoId].server.push(this);
+                this.used += input.videos[request.videoId].size;
             }
         }
     }
@@ -109,7 +116,7 @@ function Cache(id, maxSize) {
 
 function howManyDoWeUse(input) {
     var used = 0;
-    for (cache in input.caches) {
+    for (var cache in input.caches) {
         if (input.caches[cache].used > 0) {
             used++;
         }
@@ -128,9 +135,8 @@ function Input(file) {
     var cacheSize = file[0].split(" ")[4];
     var videoLine = file[1].split(" ");
     for (var elem in videoLine) {
-        this.videos.push(this.videos.length, new Video(videoLine[elem]));
+        this.videos.push(new Video(this.videos.length, videoLine[elem]));
     }
-
     mainServer.videos = this.videos;
 
     var line = 2;
@@ -143,7 +149,7 @@ function Input(file) {
             // console.log("line:", file[line]);
             var split = file[line].split(" ");
             if (endpoint.cache[split[0]] == undefined) {
-                var cache = new Cache(this.caches.length, cacheSize);
+                var cache = new Cache(Object.keys(this.caches).length, cacheSize);
                 cache.endPoints.push(endpoint);
                 endpoint.cache[split[0]] = cache;
                 this.caches[split[0]] = cache;
