0e12a8883d2022e3eb0760d202c28155a7b1c538
lewis_e <esteban.lewis@epitech.eu>
2017-02-23 20:51:40 +0100

    fill in cache

--- src/input.js
+++ src/input.js
@@ -69,6 +69,18 @@ function Cache(maxSize) {
     this.used = 0;
     this.videos = [];
     this.latency = {};
+    function fill (requests)
+    {
+        for (var request in requests)
+        {
+            if (server.capacity - server.used >= request.video.size)
+            {
+                server.videos.push(request.video);
+                request.video.servers.push(server);
+                server.used += request.video.size;
+            }
+        }
+    }
 }
 
 function Input(file) {
